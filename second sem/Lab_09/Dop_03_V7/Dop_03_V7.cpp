fd6d9d4eafb5a4ad2930d48c46f2e392bfb7f7fe
Mikhaill <mishabraz07@gmail.com>
2025-04-22 11:10:40 +0300

    зачтено

---
+++ second sem/Lab_09/Dop_03_V7/Dop_03_V7.cpp
@@ -0,0 +1,258 @@
+﻿#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <cstdlib>
+using namespace std;
+
+const unsigned int NAME_SIZE = 30;
+const unsigned int CITY_SIZE = 20;
+
+struct Address {
+    char name[NAME_SIZE];
+    char city[CITY_SIZE];
+    Address* next;
+    Address* prev;
+};
+
+int menu(void) {
+    char s[80];
+    int c;
+    cout << endl;
+    cout << "1. Ввод имени" << endl;
+    cout << "2. Удаление имени" << endl;
+    cout << "3. Вывод на экран" << endl;
+    cout << "4. Поиск" << endl;
+    cout << "5. Сохранение в файл" << endl;
+    cout << "6. Загрузка из файла" << endl;
+    cout << "7. Поиск максимального элемента" << endl;
+    cout << "8. Выход" << endl;
+    cout << endl;
+    do {
+        cout << "Ваш выбор: ";
+        cin.sync();
+        gets_s(s);
+        cout << endl;
+        c = atoi(s);
+    } while (c < 1 || c > 8);
+    return c;
+}
+
+void insert(Address* e, Address** phead, Address** plast) {
+    Address* p = *plast;
+    if (*plast == NULL) {
+        e->next = NULL;
+        e->prev = NULL;
+        *plast = e;
+        *phead = e;
+        return;
+    }
+    else {
+        p->next = e;
+        e->next = NULL;
+        e->prev = p;
+        *plast = e;
+    }
+}
+
+Address* setElement() {
+    Address* temp = new Address();
+    if (!temp) {
+        cerr << "Ошибка выделения памяти";
+        return NULL;
+    }
+    cout << "Введите имя: ";
+    cin.getline(temp->name, NAME_SIZE - 1, '\n');
+    cin.ignore(cin.rdbuf()->in_avail());
+    cin.clear();
+    cout << "Введите город: ";
+    cin.getline(temp->city, CITY_SIZE - 1, '\n');
+    cin.ignore(cin.rdbuf()->in_avail());
+    cin.clear();
+    temp->next = NULL;
+    temp->prev = NULL;
+    return temp;
+}
+
+void outputList(Address** phead, Address** plast) {
+    Address* t = *phead;
+    while (t) {
+        cout << t->name << ' ' << t->city << endl;
+        t = t->next;
+    }
+    cout << "" << endl;
+}
+
+void find(char name[NAME_SIZE], Address** phead) {
+    Address* t = *phead;
+    while (t) {
+        if (!strcmp(name, t->name)) break;
+        t = t->next;
+    }
+    if (!t)
+        cerr << "Имя не найдено" << endl;
+    else
+        cout << t->name << ' ' << t->city << endl;
+}
+
+void delet(char name[NAME_SIZE], Address** phead, Address** plast) {
+    Address* t = *phead;
+    while (t) {
+        if (!strcmp(name, t->name)) break;
+        t = t->next;
+    }
+    if (!t)
+        cerr << "Имя не найдено" << endl;
+    else {
+        if (*phead == t) {
+            *phead = t->next;
+            if (*phead) {
+                (*phead)->prev = NULL;
+            }
+            else {
+                *plast = NULL;
+            }
+        }
+        else if (t == *plast) {
+            *plast = t->prev;
+            if (*plast) {
+                (*plast)->next = NULL;
+            }
+        }
+        else {
+            t->prev->next = t->next;
+            if (t->next) {
+                t->next->prev = t->prev;
+            }
+        }
+        delete t;
+        cout << "Элемент удален" << endl;
+    }
+}
+
+void writeToFile(Address** phead) {
+    Address* t = *phead;
+    FILE* fp;
+    errno_t err = fopen_s(&fp, "mlist", "wb");
+    if (err) {
+        cerr << "Файл не открывается" << endl;
+        exit(1);
+    }
+    cout << "Сохранение в файл" << endl;
+    while (t) {
+        fwrite(t, sizeof(Address), 1, fp);
+        t = t->next;
+    }
+    fclose(fp);
+}
+
+void readFromFile(Address** phead, Address** plast) {
+    Address* t;
+    FILE* fp;
+    errno_t err = fopen_s(&fp, "mlist", "rb");
+    if (err) {
+        cerr << "Файл не открывается" << endl;
+        exit(1);
+    }
+    while (*phead) {
+        *plast = (*phead)->next;
+        delete* phead;
+        *phead = *plast;
+    }
+    *phead = *plast = NULL;
+    cout << "Загрузка из файла" << endl;
+    while (!feof(fp)) {
+        t = new Address();
+        if (!t) {
+            cerr << "Ошибка выделения памяти" << endl;
+            return;
+        }
+        if (1 != fread(t, sizeof(Address), 1, fp)) break;
+        insert(t, phead, plast);
+    }
+    fclose(fp);
+}
+
+
+Address* findMax(Address** phead, int fieldSelection) {
+    if (*phead == NULL) {
+        return NULL;
+    }
+    Address* maxElement = *phead;
+    Address* current = (*phead)->next;
+    while (current) {
+        int cmp = 0;
+        if (fieldSelection == 1) {      
+            cmp = strcmp(current->name, maxElement->name);
+        }
+        else if (fieldSelection == 2) { 
+            cmp = strcmp(current->city, maxElement->city);
+        }
+        if (cmp > 0) {
+            maxElement = current;
+        }
+        current = current->next;
+    }
+    return maxElement;
+}
+
+int main(void) {
+    system("chcp 1251");
+    Address* head = NULL;
+    Address* last = NULL;
+    while (true) {
+        switch (menu()) {
+        case 1:
+            insert(setElement(), &head, &last);
+            break;
+        case 2: {
+            char dname[NAME_SIZE];
+            cout << "Введите имя: ";
+            cin.getline(dname, NAME_SIZE - 1, '\n');
+            cin.ignore(cin.rdbuf()->in_avail());
+            cin.sync();
+            delet(dname, &head, &last);
+        }
+              break;
+        case 3:
+            outputList(&head, &last);
+            break;
+        case 4: {
+            char fname[NAME_SIZE];
+            cout << "Введите имя: ";
+            cin.getline(fname, NAME_SIZE - 1, '\n');
+            cin.ignore(cin.rdbuf()->in_avail());
+            cin.sync();
+            find(fname, &head);
+        }
+              break;
+        case 5:
+            writeToFile(&head);
+            break;
+        case 6:
+            readFromFile(&head, &last);
+            break;
+        case 7: {
+            int field;
+            cout << "Выберите поле для поиска максимального элемента:" << endl;
+            cout << "1 - имя" << endl;
+            cout << "2 - город" << endl;
+            cin >> field;
+            cin.ignore(cin.rdbuf()->in_avail());
+            Address* maxElem = findMax(&head, field);
+            if (maxElem) {
+                cout << "Максимальный элемент:" << endl;
+                cout << maxElem->name << " " << maxElem->city << endl;
+            }
+            else {
+                cout << "Список пуст или ошибка поиска." << endl;
+            }
+            break;
+        }
+        case 8:
+            exit(0);
+        default:
+            exit(1);
+        }
+    }
+    return 0;
+}
---
+++ second sem/Lab_09/Dop_2/Dop_2.cpp
@@ -0,0 +1,256 @@
+﻿#include <iostream>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <map>
+using namespace std;
+
+const int NAME_SIZE = 31;
+
+struct Student {
+    char lastName[NAME_SIZE];
+    char firstName[NAME_SIZE];
+    char patronymic[NAME_SIZE];
+    int birthYear;
+    int course;
+    int group;
+    float marks[5];
+    Student* next;
+    Student* prev;
+};
+
+void appendStudent(Student** pHead, Student** pTail, Student* newStudent) {
+    newStudent->next = nullptr;
+    newStudent->prev = nullptr;
+    if (*pTail == nullptr) {
+        *pHead = newStudent;
+        *pTail = newStudent;
+    }
+    else {
+        (*pTail)->next = newStudent;
+        newStudent->prev = *pTail;
+        *pTail = newStudent;
+    }
+}
+
+Student* createStudent() {
+    Student* s = new Student();
+    if (!s) {
+        cerr << "Ошибка выделения памяти" << endl;
+        return nullptr;
+    }
+    cout << "Введите фамилию: ";
+    cin.getline(s->lastName, NAME_SIZE);
+    cout << "Введите имя: ";
+    cin.getline(s->firstName, NAME_SIZE);
+    cout << "Введите отчество: ";
+    cin.getline(s->patronymic, NAME_SIZE);
+    cout << "Введите год рождения: ";
+    cin >> s->birthYear;
+    cout << "Введите курс: ";
+    cin >> s->course;
+    cout << "Введите номер группы: ";
+    cin >> s->group;
+    cout << "Введите оценки по 5 предметам (через пробел): ";
+    for (int i = 0; i < 5; i++) {
+        cin >> s->marks[i];
+    }
+    cin.ignore();
+    s->next = nullptr;
+    s->prev = nullptr;
+    return s;
+}
+
+void displayStudent(Student* s) {
+    cout << s->lastName << " " << s->firstName << " " << s->patronymic
+        << ", год рождения: " << s->birthYear
+        << ", курс: " << s->course
+        << ", группа: " << s->group
+        << ", оценки: ";
+    for (int i = 0; i < 5; i++) {
+        cout << s->marks[i] << " ";
+    }
+    cout << endl;
+}
+
+void printList(Student* head) {
+    Student* cur = head;
+    while (cur) {
+        displayStudent(cur);
+        cur = cur->next;
+    }
+}
+
+void sortStudents(Student** pHead, Student** pTail) {
+    vector<Student*> vec;
+    Student* cur = *pHead;
+    while (cur) {
+        vec.push_back(cur);
+        cur = cur->next;
+    }
+    sort(vec.begin(), vec.end(), [](Student* a, Student* b) -> bool {
+        if (a->course != b->course)
+            return a->course < b->course;
+        int cmp = strcmp(a->lastName, b->lastName);
+        if (cmp != 0) return cmp < 0;
+        cmp = strcmp(a->firstName, b->firstName);
+        if (cmp != 0) return cmp < 0;
+        return strcmp(a->patronymic, b->patronymic) < 0;
+        });
+    if (vec.empty()) {
+        *pHead = nullptr;
+        *pTail = nullptr;
+        return;
+    }
+    *pHead = vec[0];
+    (*pHead)->prev = nullptr;
+    for (size_t i = 1; i < vec.size(); i++) {
+        vec[i]->prev = vec[i - 1];
+        vec[i - 1]->next = vec[i];
+    }
+    *pTail = vec.back();
+    (*pTail)->next = nullptr;
+}
+
+struct GroupStats {
+    double sum[5];
+    int count;
+    GroupStats() {
+        for (int i = 0; i < 5; i++) sum[i] = 0.0;
+        count = 0;
+    }
+};
+
+void computeGroupAverages(Student* head) {
+    map<int, GroupStats> stats;
+    Student* cur = head;
+    while (cur) {
+        stats[cur->group].count++;
+        for (int i = 0; i < 5; i++) {
+            stats[cur->group].sum[i] += cur->marks[i];
+        }
+        cur = cur->next;
+    }
+    for (auto& entry : stats) {
+        int group = entry.first;
+        GroupStats& gs = entry.second;
+        cout << "Группа " << group << ":" << endl;
+        for (int i = 0; i < 5; i++) {
+            double avg = (gs.count > 0 ? gs.sum[i] / gs.count : 0);
+            cout << "  Предмет " << i + 1 << " - средний балл: " << avg << endl;
+        }
+    }
+}
+
+void findOldestYoungest(Student* head) {
+    if (!head) {
+        cout << "Список студентов пуст." << endl;
+        return;
+    }
+    Student* oldest = head;
+    Student* youngest = head;
+    Student* cur = head;
+    while (cur) {
+        if (cur->birthYear < oldest->birthYear)
+            oldest = cur;
+        if (cur->birthYear > youngest->birthYear)
+            youngest = cur;
+        cur = cur->next;
+    }
+    cout << "Самый старший студент:" << endl;
+    displayStudent(oldest);
+    cout << "Самый младший студент:" << endl;
+    displayStudent(youngest);
+}
+
+void findBestStudentsPerGroup(Student* head) {
+    map<int, pair<Student*, float>> best;
+    
+    Student* cur = head;
+    
+    while (cur) {
+        float avg = 0;
+       
+        for (int i = 0; i < 5; i++) {
+            avg += cur->marks[i];
+        }
+        avg /= 5;
+        if (best.find(cur->group) == best.end() || avg > best[cur->group].second)
+            best[cur->group] = make_pair(cur, avg);
+       
+        cur = cur->next;
+       
+    }
+    for (auto& entry : best) {
+        int group = entry.first;
+       
+        Student* bestStudent = entry.second.first;
+       
+        float bestAvg = entry.second.second;
+       
+        cout << "Группа " << group << " - лучший студент: " << bestStudent->lastName << " "
+            << bestStudent->firstName << " " << bestStudent->patronymic
+            << " со средним баллом: " << bestAvg << endl;
+    }
+}
+
+
+int main() {
+    system("chcp 1251");
+    Student* head = nullptr;
+    Student* tail = nullptr;
+    int choice;
+    do {
+        cout << "\nМеню:" << endl;
+        cout << "1. Добавить студента" << endl;
+        cout << "2. Вывести список студентов" << endl;
+        cout << "3. Отсортировать студентов (по курсу и алфавиту)" << endl;
+        cout << "4. Вывести средний балл каждой группы по каждому предмету" << endl;
+        cout << "5. Найти самого старшего и самого младшего студента" << endl;
+        cout << "6. Найти лучшего студента каждой группы" << endl;
+        cout << "7. Выход" << endl;
+        cout << "Ваш выбор: ";
+        cin >> choice;
+        cin.ignore();
+        switch (choice) {
+        case 1: {
+            Student* s = createStudent();
+            appendStudent(&head, &tail, s);
+            break;
+        }
+        case 2: {
+            printList(head);
+            break;
+        }
+        case 3: {
+            sortStudents(&head, &tail);
+            cout << "Список студентов отсортирован." << endl;
+            break;
+        }
+        case 4: {
+            computeGroupAverages(head);
+            break;
+        }
+        case 5: {
+            findOldestYoungest(head);
+            break;
+        }
+        case 6: {
+            findBestStudentsPerGroup(head);
+            break;
+        }
+        case 7:
+            cout << "Выход из программы." << endl;
+            break;
+        default:
+            cout << "Неверный выбор, попробуйте ещё раз." << endl;
+        }
+    } while (choice != 7);
+    Student* cur = head;
+    while (cur) {
+        Student* next = cur->next;
+        delete cur;
+        cur = next;
+    }
+    return 0;
+}
